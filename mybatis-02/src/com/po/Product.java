d1f6af339ae1615aa96f32a90a3a4a213db15228
liao1024 <153038066@qq.com>
2021-04-09 19:09:45 +0800

    2021/4/9

---
+++ mybatis-02/src/com/po/Product.java
@@ -0,0 +1,52 @@
+package src.com.po;
+
+import java.util.List;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private double price;
+    private List<Orders> orders;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Orders> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Orders> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", orders=" + orders +
+                '}';
+    }
+}
